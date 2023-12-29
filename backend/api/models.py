6dc0610d3bdb2ae238ba878a028f2f0e892575f0
MyName <nickmol96@hotmail.com>
2023-12-29 19:23:31 +0100

    check field

--- backend/api/models.py
+++ backend/api/models.py
@@ -14,6 +14,7 @@ class Project(models.Model):
     start_date = models.DateField()
     end_date = models.DateField()
     comments = models.CharField(max_length=500, blank=True, null=True)
+    check1 = models.CharField(max_length=500, blank=True, null=True)
     status = models.CharField(max_length=100)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
